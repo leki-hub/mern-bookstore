d044b5de58bb029579696978cc73617a7a0e4c67
Leki <lekisintole82@gmail.com>
2023-10-26 09:40:59 +0300

    testing

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,11 @@
 import express from "express"
-import { PORT } from "./config";
+import { PORT } from "./config.js";
 const app = express();
 
+app.get('/',(request,response)=>{
+    console.log("Hello World");
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
