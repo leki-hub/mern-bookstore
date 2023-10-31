10ab34b10ad10d5398de1738ef821f251a4ddb01
Leki <lekisintole82@gmail.com>
2023-10-31 22:47:17 +0300

    pages

---
+++ frontend/src/pages/DeleteBook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DeleteBook = () => {
+  return (
+    <div>
+      <h1>yES</h1>
+    </div>
+  )
+}
+
+export default DeleteBook
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState,useEffect } from 'react'
+import axios from "axios"
+
+const Home = () => {
+
+
+
+
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default Home
