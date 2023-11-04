c52c9aafd95774599e8b357b78808277f975e89c
Leki <lekisintole82@gmail.com>
2023-11-04 16:34:58 +0300

    delete page

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const DeleteBook = () => {
-  return (
-    <div>
-      <h1>yES</h1>
-    </div>
-  )
-}
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-export default DeleteBook
+  const handleDelete = async () => {
+    setLoading(true);
+    try {
+      await axios.delete(`http://localhost:5554/books/${id}`);
+      setLoading(false);
+      navigate('/'); 
+    } catch (error) {
+      console.error('Error', error);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="p-4">
+      <button
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleDelete}
+        disabled={loading}
+      >
+        {loading ? 'Deleting...' : 'Delete Book'}
+      </button>
+    </div>
+  );
+};
+
+export default DeleteBook;
+
