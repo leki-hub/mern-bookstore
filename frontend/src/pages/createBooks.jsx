da29e93e0d6fecfe45d46c82cd7018dc68d6c0b3
Leki <lekisintole82@gmail.com>
2023-11-04 11:46:44 +0300

    navigate

--- frontend/src/pages/createBooks.jsx
+++ frontend/src/pages/createBooks.jsx
@@ -16,22 +16,37 @@ const CreateBooks = () => {
    }
 
   const handleSubmit = (e) =>{
-
     e.preventDefault()
     axios.post('http://localhost:5554/books',data)
-    .then(res=>{
-      
+    .then(()=>{
+      setLoading(false)
       console.log("book created")
+      setTimeout(()=>{navigate('/')},2000)
     })
     .catch(err=>console.log(err))
   }
   return (
-    <div>
+    <div className='mx-4'>
 
       <h1>Create Books</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Enter Title' value={title} onChange={e=>setTitle(e.target.value)} />
+        <div my-4>
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
+        <input type="text" placeholder='Enter Title' value={title} onChange={e=>setTitle(e.target.value)}  className='border-2 border-gray-500 px-4 py-2'/>
+        </div>
+        <div className='my-4'>
+        <label className='text-xl mr-4 text-gray-500'>Author</label>
+           <input type="text" placeholder='Enter Author Name' value={author} onChange={e=>setAuthor(e.target)}  className='border-2 border-gray-500 px-4 py-2'/>
+        </div>
+       
+        <div className='my-4'>
+        <label className='text-xl mr-4 text-gray-500'>Publish Year</label>
+        <input type="text" placeholder='Enter Author Name' value={publishYear} onChange={e=>setYear(e.target)} className='border-2 border-gray-500 px-4 py-2' />
 
+        </div>
+       
+     
+        
       </form>
     </div>
   )
