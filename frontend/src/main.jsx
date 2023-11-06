53312356b1b89857db84ddbeccfe0eca1ea670d8
Leki <lekisintole82@gmail.com>
2023-11-06 08:38:52 +0300

    tailwind import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { SnackbarProvider, useSnackbar } from 'notistack';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+  <SnackbarProvider>
     <App />
-  </>,
+  </SnackbarProvider>,
 )
