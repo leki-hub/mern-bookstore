6d48c9a5888acdb1d14808f52a9a8e27e26af344
Leki <lekisintole82@gmail.com>
2023-11-06 11:18:23 +0300

    usesnackbar removed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { SnackbarProvider, useSnackbar } from 'notistack';
+import { SnackbarProvider} from 'notistack';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <SnackbarProvider>
